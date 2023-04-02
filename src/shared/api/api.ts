3f949fa512fff35d9df1ad398ab67a088596af0d
Evgenia <evpob@mail.orb.ru>
2023-04-02 09:28:51 +0500

    netlify, api conf

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -1,11 +1,13 @@
 import { USER_LOCAL_STORAGE_KEY } from 'shared/const/localStorage';
 import axios from 'axios';
 
-const baseUrl = __IS_DEV__ ? 'http://localhost:8000' : 'https://production.ru';
-
 export const $api = axios.create({
-    baseURL: baseUrl,
-    headers: {
-        authorization: localStorage.getItem(USER_LOCAL_STORAGE_KEY) || '',
+    baseURL: __API__,
+});
+
+$api.interceptors.request.use((config) => {
+    if (config.headers) {
+        config.headers.authorization = localStorage.getItem(USER_LOCAL_STORAGE_KEY) || ''; 
     }
+    return config;
 });
